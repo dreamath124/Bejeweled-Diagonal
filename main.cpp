7ed28c0e7061a6d2c6dacbdf07fc8e087164fdb0
Bui Duc Dang <22021155@vnu.edu.vn>
2023-04-29 15:05:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include "Window.h"
+#include "Menu.h"
+#include "Utilities.h"
+enum Context{
+    MENU,
+    GAME
+};
 
 int main(int argc, char* argv[])
 {
-    std::string appIconLocation = "appIcon.bmp";
-    SDL_Window* window = SDL_CreateWindow("Bejeweled Diagonal", 
-        SDL_WINDOWPOS_CENTERED, 
-        SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-    SDL_Surface* appIcon = SDL_LoadBMP(appIconLocation.c_str());
-    SDL_SetWindowIcon(window, appIcon);
-    SDL_Delay(1000);
+    Window window;
+    Game game;
+    game.loadImageGallery(window.renderer);
+    Menu menu(window.renderer, game);
+    bool isRunning = true;
+    Context context = MENU;
+    while(isRunning)
+    {
+        SDL_Event e;
+        while(SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+            {
+                isRunning = false;
+                break;
+            }
+            if (context == GAME)
+                game.handleEvent(&e);
+            else
+                if (menu.handleEvent(&e))
+                    context = GAME;
+        }
+        if (context == GAME) 
+            game.render(window.renderer); 
+        else {
+            menu.show();
+            SDL_SetRenderDrawColor(window.renderer, 0, 0, 0, 255);
+        }
+        SDL_RenderPresent(window.renderer);
+        SDL_Delay(16);
+    }
+    SDL_Quit();
     return 0;
 }
